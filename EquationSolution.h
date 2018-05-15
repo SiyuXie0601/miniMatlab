0dd878ba7c15015c173d935929d6d034e213280a
SiyuXie0601 <siyu0601@126.com>
2018-05-15 18:20:40 +0800

    add some comments

--- EquationSolution.h
+++ EquationSolution.h
@@ -8,7 +8,7 @@ public:
     EquationSolution();
     static String solveLnrEqtnWithOneUkn(const String& exp);//解一元一次方程
     static String solveQdrtcEqtnWithOneUkn(const String& exp);//解一元二次方程
-    static String solveCbcEqtnWithOneUkn(const String& exp);
+    static String solveCbcEqtnWithOneUkn(const String& exp);//解一元三次方程
     static String solveLnrEqtnWithTwoUkn(const String& exp);
 };
 
