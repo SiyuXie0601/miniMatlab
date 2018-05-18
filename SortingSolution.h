1a3b2bec2324d32a9b3c42a3a1c811bb8015a7f7
SiyuXie0601 <siyu0601@126.com>
2018-05-18 20:09:41 +0800

    add comment

--- SortingSolution.h
+++ SortingSolution.h
@@ -10,7 +10,7 @@ public:
             Ascend, Descend//0为升序，1为降序
         };
     SortingSolution();
-    static String sort(const vector &arr, Mode mode);//将arr中的数以mode方式排序，并将结果以字符串方式返回，元素与元素之间用逗号隔开(最后一个元素后无需,)，例如 "1,2,3,4"
+    static String sort(const vector &arr, Mode mode);//将arr中的数以mode方式排序，并将结果以字符串方式返回，元素与元素之间用逗号隔开(最后一个元素后无需,)，例如 "1,2,3.5,4"
 };
 
 #endif // SORTINGSOLUTION_H
